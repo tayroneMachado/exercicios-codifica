5146dddc43f843c63204bce7b0fdee489f0a963c
tayroneMachado <tatank_@live.com>
2025-05-01 20:56:46 -0300

    ex 09

---
+++ Lista-exercicios-01/09.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')()
+// 9. Implemente um programa que exibe uma contagem regressiva de 10 até 1 no console
+// utilizando um loop for.
+
+for(i = 10; i > 0; i--){
+    console.log(i)
+}
